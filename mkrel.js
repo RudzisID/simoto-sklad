741a3ec6a4a6f68e8d45c87729449421639163fa
RudzisID <rudzis.ilya@gmail.com>
2026-04-27 21:37:53 +0700

    Fix release script

--- mkrel.js
+++ mkrel.js
@@ -7,22 +7,14 @@ const token = fs.readFileSync('.env', 'utf8').match(/GH_TOKEN=(.+)/)[1];
 
 console.log('Release:', tag);
 
+// Create release
 const data = JSON.stringify({tag_name: tag, name: 'SiMOTO ' + tag, draft: false});
 const req = https.request({
     hostname: 'api.github.com',
     path: '/repos/RudzisID/simoto-sklad/releases',
     method: 'POST',
     headers: {'Authorization': 'token '+token, 'Content-Type': 'application/json', 'User-Agent': 'SiMOTO'}
-}, res => { 
-    let d = ''; 
-    res.on('data', c => d += c); 
-    res.on('end', () => {
-        console.log('Status:', res.statusCode);
-        if (res.statusCode === 201) {
-            console.log('OK! ' + tag + ' created');
-        }
-    });
-});
-req.on('error', e => console.log('Error:', e.message));
+}, res => { let d=''; res.on('data',c=>d+=c); res.on('end', () => console.log(res.statusCode, d.substring(0,80))) });
+req.on('error', e => console.log('E:', e.message));
 req.write(data);
 req.end();
